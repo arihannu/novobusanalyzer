318faee9c5ea4df4b3a978ca224ef5088f95b4d6
Ari Hannula <ari.hannula@gmail.com>
2025-08-11 12:58:52 +0200

    Fix SimulatedSystems batch scripts

--- Sources/BUSMASTER/Application/SimulatedSystems/include/MakeFile_CAN.bat
+++ Sources/BUSMASTER/Application/SimulatedSystems/include/MakeFile_CAN.bat
@@ -2,4 +2,5 @@ del libWrapper_CAN.a
 g++ -fpermissive -DNO_INCLUDE_TEMPSTRUCT -c  Trigonometric.cpp BMCANDefines.cpp BMUtility.cpp StringFunctions.cpp FileOperations.cpp   OtherCaplFunc.cpp CANCAPLWrapper.cpp Wrapper_Common.cpp 
 ar -rcs libWrapper_CAN.a Trigonometric.o StringFunctions.o BMCANDefines.o BMUtility.o OtherCaplFunc.o FileOperations.o  CANCAPLWrapper.o  Wrapper_Common.o
 del Trigonometric.o StringFunctions.o BMCANDefines.o BMUtility.o OtherCaplFunc.o FileOperations.o  CANCAPLWrapper.o  Wrapper_Common.o
+md ..\OBJ
 copy /Y libWrapper_CAN.a ..\OBJ\
--- Sources/BUSMASTER/Application/SimulatedSystems/include/MakeFile_LIN.bat
+++ Sources/BUSMASTER/Application/SimulatedSystems/include/MakeFile_LIN.bat
@@ -1,5 +1,6 @@
 del libWrapper_LIN.a
 g++ -DNO_INCLUDE_TEMPSTRUCT -c  BMLINDefines.cpp BMUtility.cpp Wrapper_Common.cpp 
 ar -rcs libWrapper_LIN.a BMLINDefines.O BMUtility.o Wrapper_Common.o
+md ..\OBJ
 copy /Y libWrapper_LIN.a ..\OBJ\
 del BMLINDefines.O BMUtility.o Wrapper_Common.o
